c9c1e86670599ad4548b28f7bb9b4839babd3ea9
Евгений <ssvenom82@gmail.com>
2026-06-11 15:36:48 +0300

    Циклы while и for. оператоторы break и continue

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-    UserManager manager = new UserManager();
-    manager.startApp();
+        int balance = 450;
+        int fee = 100;
+
+        for (int i = 1; i <= 5; i++) {
+            balance -= 100;
+            if (balance < 100) {
+                System.out.println("Деньги кончились, прерываем работу! ");
+                break;
+            }
+            System.out.println("Этаж " + i + " проверен. ");
+
+
+
+        }
 
 
     }
