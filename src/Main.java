6b4367c1a5abc16ac34f7e2032fa5611eea28d2e
Евгений <svenom82@gmail.com>
2026-06-19 15:21:38 +0300

    Practice with string methods

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        String branch = getBranchName("java-touchBoard-main");
-        System.out.println(" Результат работы метода: " + branch);
+       String fileName = "Avatar.SVG";
+       String url = " MY-FIGMA-LINK.COM/PROJECT1 ";
+       String link ="https://www.figma.com/design/6u8tWHfm6jHnuO3YT0xTCT";
+       U.pl("Исправленная строка " + cleanUrl(url));
+       U.pl("Это верная ссылка на фигму " + isFigmaLink(link));
 
     }
 
-    public static String getBranchName(String repo) {
-        int lastSlash = repo.lastIndexOf("-");
+    public static boolean isPngFile(String fileName) {
+        if (fileName.endsWith(".png")) {
+            U.pl("Файл правильного формата! ");
+        } else {
+            U.pl(" Не верный формат");
+        }return fileName.endsWith(".png");
 
-        if (lastSlash != -1) {
-            String result = repo.substring(lastSlash + 1);
-            return result;
-
-        }
-        return repo;
     }
+
+    public static String cleanUrl (String url) {
+        url = url.trim();
+        url = url.toLowerCase();
+        return url;
+    }
+    public static boolean isFigmaLink (String link) { boolean result = link.contains("figma.com"); return result; }
+
+
 
 }
