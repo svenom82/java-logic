41342670c6ba4b70a1dc2b7adf67267ee5c55bd4
Евгений <svenom82@gmail.com>
2026-06-26 20:15:24 +0300

    Практика

--- src/TaskDemo.java
+++ src/TaskDemo.java
@@ -1,23 +1,18 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class TaskDemo {
 
     public static void main(String[] args) {
-        Status[] statuses = new Status[]{Status.SUCCESS, Status.ERROR, Status.WARNING};
-        System.out.println(Array(statuses));
+        String[] list = {"Колодец ", "Собака ", "Корова " };
+        System.out.println(mergeText(list));
+    }
+    public static String mergeText(String[] list){
+        String line = "";
+
+        for (int i = 0; i < list.length; i++) {
+            line = line + list[i];
+        } return  line;
     }
 
-    public static int Array(Status[] arr) {
-        int count = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == Status.ERROR) {
-                count++;
-            }
-        }
-        return count;
-
-    }
 
 }
