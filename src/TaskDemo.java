7a4dd3e3e765f77a319fc67c0ac9e1ea8f747e85
Евгений <svenom82@gmail.com>
2026-06-28 11:45:26 +0300

    Практика

--- src/TaskDemo.java
+++ src/TaskDemo.java
@@ -3,16 +3,25 @@ import java.util.Arrays;
 public class TaskDemo {
 
     public static void main(String[] args) {
-        String[] list = {"Колодец ", "Собака ", "Корова " };
-        System.out.println(mergeText(list));
-    }
-    public static String mergeText(String[] list){
-        String line = "";
 
-        for (int i = 0; i < list.length; i++) {
-            line = line + list[i];
-        } return  line;
+        int[] yearUser = new int[]{2010, 1995, 2008, 2024, 2015, 1990};
+        System.out.println("Совершеннолетних: " + yearUserCount(yearUser));
     }
 
+    public static int yearUserCount(int[] yearUser) {
+        int currentUser = 2026;
+        int count = 0;
 
+        for (int i = 0; i < yearUser.length; i++) {
+            int age = currentUser - yearUser[i];
+
+            if (age >= 18) {
+                count++;
+            }
+
+
+        }
+        return count;
+
+    }
 }
