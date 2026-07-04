fe54534128707228127ec4b73e39ca329e759709
Евгений <svenom82@gmail.com>
2026-07-04 13:56:56 +0300

    Практика

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,11 @@
 public class Main {
     public static final int BASE_GRID = 8;
 
-    enum OrderStatus {
-        DRAFT,
-        PROCESSING,
-        DELIVERED
-    }
+
 
     public static void main(String[] args) {
-        OrderStatus currentStatus = OrderStatus.DRAFT;
-
-       getStatusColor(currentStatus);
     }
 
-    public static void getStatusColor(OrderStatus status) {
-        switch (status) {
-            case DRAFT -> U.pl("Пользователь начал оформление, плашка серая");
-            case PROCESSING -> U.pl("Заказ улетел на склад, плашка желтая");
-            case DELIVERED -> {
-                U.pl("Заказ успешно дошел до клиента");
-                U.pl("Красим плашку в зеленый и шлем пуш");
-            }
-        }
-    }
+
 
 }
--- src/Status.java
+++
@@ -1,3 +0,0 @@
-enum Status {
-    LOW, MEDIUM, HIGH
-}
